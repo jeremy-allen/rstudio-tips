590afcf25e1c8abea50138e754ee875f621933d3
Jeremy Allen <jeremy.allen@rstudio.com>
2021-11-02 20:49:55 +0000

    1-3 rmd files for demo and images added

--- scratch.R
+++ scratch.R
@@ -1,22 +1,6 @@
-# test git push
-
-library(palmerpenguins)
-library(tidyverse)
-library(janitor)
-
-# rearrange panels
-# change theme
-# default R version
-# don't save workspace data
-# show last value
-# set repo for RStudio Public Package Manager
-# allow scroll past end of doc
-# rainbow parentheses
-# show current memory usage
-# shift-cmd-p then do anything
-# syntax highlighting in console output
-
-# shift-cmd-r to insert section break and call it "prep"
+# change shortcut  for insert section break to ctl-shift-b
+# insert section break and call it "prep"
+# normally is shift-cmd-r but that will refresh your browser
 
 
 
