ccb21df904e28ec1312f90113c5fe416d1150a73
Jeremy Allen <jeremy.allen@rstudio.com>
2021-11-02 13:36:02 +0000

    starting list if ide tips

---
+++ scratch.R
@@ -0,0 +1,76 @@
+library(palmerpenguins)
+library(tidyverse)
+library(janitor)
+
+# rearrange panels
+# change theme
+# default R version
+# don't save workspace data
+# show last value
+# set repo for RStudio Public Package Manager
+# allow scroll past end of doc
+# rainbow parentheses
+# show current memory usage
+# shift-cmd-p then do anything
+# syntax highlighting in console output
+
+# shift-cmd-r to insert section break and call it "prep"
+
+
+
+df <- penguins_raw
+
+df
+
+# gross!
+names(df)
+
+# clean those names
+df <- df %>% 
+  clean_names()
+
+# snippet time! type "if" then hit shift-tab
+
+
+
+# regex time!
+# reset df
+df <- penguins_raw
+
+# are there spaces or capital letters in col names?
+str_detect(names(df), "[\\sA-Z]")
+# but we only want one answer, so wrap in any()
+any(str_detect(names(df), "[\\sA-Z]"))
+
+# snippet time! type "if" then hit shift-tab
+# paste in our regex condition and our clean_names code
+
+
+
+
+
+
+glimpse(df)
+
+df %>% 
+  summarise(across(ends_with("mm"), ~mean(., na.rm = TRUE)))
+
+df %>% 
+  summarise(across(ends_with("mm"), ~mean(., na.rm = TRUE)))
+
+df %>% 
+  summarise(across(starts_with("culmen"), mean, na.rm = TRUE))
+
+df %>% 
+  summarise(
+    across(
+      starts_with("culmen"),
+      mean,
+      na.rm = TRUE
+      )
+    )
+
+
+
+
+
