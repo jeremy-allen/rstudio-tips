1088710a7d6975f53cc78973cf9a21390dc70113
Jeremy Allen <jeremy.allen@rstudio.com>
2021-11-05 17:03:37 +0000

    added jobs section and removed an errant gif

--- scratch.R
+++ scratch.R
@@ -1,62 +1,12 @@
-# change shortcut  for insert section break to ctl-shift-b
-# insert section break and call it "prep"
-# normally is shift-cmd-r but that will refresh your browser
-
-
+library(palmerpenguins)
+library(dplyr)
+library(janitor)
 
 df <- penguins_raw
-
-df
-
-# gross!
-names(df)
 
 # clean those names
 df <- df %>% 
   clean_names()
 
-# snippet time! type "if" then hit shift-tab
-
-
-
-# regex time!
-# reset df
-df <- penguins_raw
-
-# are there spaces or capital letters in col names?
-str_detect(names(df), "[\\sA-Z]")
-# but we only want one answer, so wrap in any()
-any(str_detect(names(df), "[\\sA-Z]"))
-
-# snippet time! type "if" then hit shift-tab
-# paste in our regex condition and our clean_names code
-
-
-
-
-
-
-glimpse(df)
-
-df %>% 
+mean_sum <- df %>% 
   summarise(across(ends_with("mm"), ~mean(., na.rm = TRUE)))
-
-df %>% 
-  summarise(across(ends_with("mm"), ~mean(., na.rm = TRUE)))
-
-df %>% 
-  summarise(across(starts_with("culmen"), mean, na.rm = TRUE))
-
-df %>% 
-  summarise(
-    across(
-      starts_with("culmen"),
-      mean,
-      na.rm = TRUE
-      )
-    )
-
-
-
-
-
